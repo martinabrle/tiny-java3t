bf65fdffa1e5a0989fed39639a536af1bc690c2a
martinabrle <martinabrle@users.noreply.github.com>
2022-09-17 12:18:13 +0200

    test

--- todoapi/src/main/java/app/demo/todoapi/DatabaseLoader.java
+++ todoapi/src/main/java/app/demo/todoapi/DatabaseLoader.java
@@ -20,119 +20,118 @@ public class DatabaseLoader implements CommandLineRunner {
         @Autowired
         private AppConfig appConfig;
 
-        @Autowired 
+        @Autowired
         public DatabaseLoader(TodoRepository repository) {
                 todoRepository = repository;
         }
 
         @Override
         public void run(String... strings) throws Exception {
-                //TODO: uncomment
-                //if (loadDemoData()) {
+                if (loadDemoData()) {
                         LOGGER.debug("Loading default data");
                         DatabaseLoader.initRepoWithDemoData(todoRepository);
-                //} else {
+                } else {
                         LOGGER.debug("Skipping demo data load");
-                //}
+                }
         }
 
         public static void initRepoWithDemoData(TodoRepository todoRepository) {
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000001"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000001"), 
-                                        "Create Stark Enterprises",
-                                        new Date(), 
-                                        null));
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000001"),
+                                                        "Create Stark Enterprises",
+                                                        new Date(),
+                                                        null));
                 }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000002"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000002"), 
-                                        "Invent the first Iron Man Suit",
-                                        new Date(), 
-                                        null));
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000002"),
+                                                        "Invent the first Iron Man Suit",
+                                                        new Date(),
+                                                        null));
                 }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000003"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000003"), 
-                                        "Become a Hero",
-                                        new Date(), 
-                                        null));
-                }               
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000003"),
+                                                        "Become a Hero",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000004"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000004"), 
-                                        "Help build S.H.I.E.L.D.",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000004"),
+                                                        "Help build S.H.I.E.L.D.",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000005"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000005"), 
-                                        "Form the Avengers",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000005"),
+                                                        "Form the Avengers",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000006"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000006"), 
-                                        "Put Hawkeye on the right path",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000006"),
+                                                        "Put Hawkeye on the right path",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000007"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000007"), 
-                                        "Make Stark Industries a massive success",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000007"),
+                                                        "Make Stark Industries a massive success",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000008"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000008"), 
-                                        "Keep escaping death in the most Tony Stark way possible",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000008"),
+                                                        "Keep escaping death in the most Tony Stark way possible",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000009"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000009"), 
-                                        "Learn Spring boot",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000009"),
+                                                        "Learn Spring boot",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000010"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000010"), 
-                                        "Deploy a multi tier Spring boot app into Azure",
-                                        new Date(), 
-                                        null));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000010"),
+                                                        "Deploy a multi tier Spring boot app into Azure",
+                                                        new Date(),
+                                                        null));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000011"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000011"), 
-                                        "Make a hash of everything",
-                                        new Date(), 
-                                        new Date()));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000011"),
+                                                        "Make a hash of everything",
+                                                        new Date(),
+                                                        new Date()));
+                }
                 if (!todoRepository.existsById(UUID.fromString("00000000-0000-0000-0000-000000000012"))) {
                         todoRepository.save(
-                                new app.demo.todoapi.entity.Todo(
-                                        UUID.fromString("00000000-0000-0000-0000-000000000012"), 
-                                        "Ask Petteri to fix it",
-                                        new Date(), 
-                                        new Date()));
-                } 
+                                        new app.demo.todoapi.entity.Todo(
+                                                        UUID.fromString("00000000-0000-0000-0000-000000000012"),
+                                                        "Ask Petteri to fix it",
+                                                        new Date(),
+                                                        new Date()));
+                }
         }
 
         private boolean loadDemoData() {
